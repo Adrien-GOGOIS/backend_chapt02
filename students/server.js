6751ebc5676b6ced45c9efc0bdf49ff857ab9585
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-08 13:44:21 +0100

    Create 2 path to get students and create new one

--- students/server.js
+++ students/server.js
@@ -1,2 +1,29 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+// Confirmation de requête :
+app.use((req, res, next) => {
+  console.log("requête reçu");
+  next(); //
+});
+
+// Tableau données API
+const students = [];
+
+// Routes liste étudiants
+app.get("/students", (req, res) => {
+  res.json(students);
+});
+
+// Post nouvel étudiant :
+app.post("/students/", (req, res) => {
+  students.push({
+    id: students.length + 1,
+    name: req.body.name,
+  });
+  res.send(students);
+});
+
+// Démarrage serveur
+app.listen(8000, console.log("Listening......"));
