0829f5bc5c02f745cf08119c56e46c1585330981
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-10 11:03:03 +0100

    Add React app to create new students and display it

--- students_api/src/views/Home.js
+++ students_api/src/views/Home.js
@@ -5,37 +5,49 @@ const axios = require("axios");
 export default function Home() {
   const [student, setStudent] = useState();
   const [studentsList, setStudentsList] = useState();
+  const [reload, setReload] = useState(false);
 
   useEffect(() => {
     axios.get("http://localhost:8000/students").then((res) => {
       const data = res.data;
       setStudentsList(data);
+      console.log("TEST axios", studentsList);
     });
-  }, []);
+  }, [reload]);
 
   const onChange = (e) => {
     setStudent(e.target.value);
+    // console.log("INPUT", student);
   };
 
-  const onSubmit = () => {
-    axios.post();
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:8000/students", {
+      id: studentsList.length + 1,
+      name: student,
+    });
+
+    setReload((prev) => !prev);
   };
 
   return (
     <>
       <h1>Liste des étudiants</h1>
-
-      {studentsList.map((student) => {
-        <ul>
-          <li>{studentsList.name}</li>
-        </ul>;
-      })}
-      <form action="POST">
+      <form>
         <input type="text" onChange={onChange} />
         <button type="submit" onClick={onSubmit}>
           SUBMIT
         </button>
       </form>
+      {studentsList &&
+        studentsList.map((stu) => {
+          return (
+            <ul>
+              <li>{stu.name}</li>
+            </ul>
+          );
+        })}
     </>
   );
 }
